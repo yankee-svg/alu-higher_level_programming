84dbeee1be177fc375bc881f85215a72a5c90e6d
yankee-svg <e.barame60@gmail.com>
2024-10-24 11:50:43 +0200

    slower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z'8-uppercase.py
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
