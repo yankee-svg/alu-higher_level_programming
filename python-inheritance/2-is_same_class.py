d351b3550dabf98197920b98e4f9de9b80edc75e
yankee-svg <e.barame60@gmail.com>
2024-11-09 06:20:54 +0200

    check if is a subclass

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' true or false if the objects is exactly an instance '''
+
+
+def is_same_class(obj, a_class):
+    ''' return true if object is instance '''
+
+    return type(obj) is a_class
