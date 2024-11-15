d04bc5cbb1c1b77fb76a4fd46314fe33a8d10d26
yankee-svg <e.barame60@gmail.com>
2024-11-15 19:14:14 +0200

    mport the database dump from hbtn_0d_tvshows

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,6 @@
 -- Import the database dump from hbtn_0d_tvshows
-SELECT tv_genres.name
-FROM tv_genres
-     JOIN tv_show_genres
-     	  ON tv_genres.id = tv_show_genres.genre_id
-     JOIN tv_shows
-     	  ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = "Dexter"
-ORDER BY tv_genres.name;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_show_genres
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC, tv_genres.id ASC;
