8ada9e997e06f698e5363f3befc6f822d244e09a
yankee-svg <e.barame@alustudent.com>
2024-11-24 10:36:27 +0200

    sends to url

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Displays the header"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    print(req.headers.get("X-Request-Id"))
