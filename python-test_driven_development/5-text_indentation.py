0d65c7543199c31b0d6aa9b253f245895be80a8a
yankee-svg <e.barame@alustudent.com>
2024-11-27 09:04:42 +0200

    indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""""text_indentation"""
+
+
+def text_indentation(text):
+    """ text indentation
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
